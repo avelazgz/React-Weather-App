41a44a527bc0529af9e3cca1b0591972da412f81
Antonio Vela Aparicio <antonio.vela@plainconcepts.com>
2023-01-25 16:32:19 +0100

    Deleted everything

--- app/tests/sanity.spec.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-var Nav = require('../components/Nav.jsx');
-
-describe('Sanity Check', function () {
-	it('should be able to run mocha and use chai for assertions, and import JSX', function () {
-		assert.ok(true);
-	});
-});
